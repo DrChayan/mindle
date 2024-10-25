13974cca9dff3daa881365498b4bc19967ddb7e3
matimessen <matilde.messen123@gmail.com>
2024-10-25 15:31:37 -0300

    Nuevas apps y base de datos :D

---
+++ diccionario/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class trastorno(models.Model):
+    nombre=models.CharField(max_length=200)
+    contexto=models.CharField(max_length=300)
+    
+class sintomas(models.Model):
+    trastorno=models.ForeignKey('trastorno', on_delete=models.CASCADE, default=0)
+    sintoma1=models.CharField(max_length=100)
+    sintoma2=models.CharField(max_length=100)
+    sintoma3=models.CharField(max_length=100)
+    sintoma4=models.CharField(max_length=100)
+    sintoma5=models.CharField(max_length=100)
+
+
+
