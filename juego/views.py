c0c51fb3aab38f02fcc0c45309f48bea59f320ec
Miloomor <ejmv70@gmail.com>
2024-11-28 17:41:42 -0300

    prototipo E-I

--- juego/views.py
+++ juego/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from diccionario.models import trastorno 
+import random
 
 # Create your views here.
 def game(request):
     return render(request, 'game.html')
- 
+
+def obtenerTrastornoAleatorio(request):
+    trastornoAleatorio = trastorno.objects.filter(seleccionada=False).order_by("?").first()
+
+    if trastorno:
+        trastornoAleatorio.seleccionada = True
+        trastornoAleatorio.save()
+        Trastorno = trastornoAleatorio.texto
+    else:
+        pass
+
+    return render(request, 'game.html', {'Trastorno': Trastorno})
