8dd4f1f00a91c82f775d43ba2a87bc649242cf39
matimessen <matilde.messen123@gmail.com>
2024-11-29 12:34:22 -0300

    error game

--- juego/views.py
+++ juego/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def game(request):
-    return render(request, 'mindle/templates/game.html')
+    return render(request, '../templates/game.html')
  
