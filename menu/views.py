4a5fee8083ad6e65fb37247cf98c98b44a88dff7
DrChayan <ismovaldes@gmail.com>
2024-12-01 22:56:01 -0300

    diccionario, juego, ranking y puntaje

--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from juego.models import Puntaje
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'user': request.user})
+        # Obtener el ranking de los 10 jugadores con mayor puntaje
+    ranking = Puntaje.objects.all().order_by('-puntaje')[:5]
+
+    return render(request, 'index.html', {'user': request.user, 'ranking': ranking})
+
 
 def login(request):
     return render(request, 'inicioDeSesion.html')
